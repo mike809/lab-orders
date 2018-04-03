d13ee2254607b48b573adc74b9579c22499c5e8b
Michael Romano <micha7a.com@gmail.com>
2018-04-03 12:42:27 -0400

    Remove comments and change quotes on routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,14 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:edit, :update]
 
-  # GET /orders
-  # GET /orders.json
   def index
     @orders = Order.all
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
-
-  # GET /orders/new
-  def new
-    @order = Order.new
   end
 
   # GET /orders/1/edit
   def edit
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.new(order_params)
 
@@ -37,8 +23,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
     respond_to do |format|
       if @order.update(order_params)
@@ -51,8 +35,7 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
+  # Soft delete
   def destroy
     @order.destroy
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'orders#index'
 
-  resources :orders
+  resources :orders, only: [:index, :edit, :update, :delete]
   get 'azure_oauth2/callback'
 
   devise_scope :user do
-    get "login", to: "devise/sessions#new"
-    post "logout", to: "devise/sessions#destroy"
+    get 'login', to: 'devise/sessions#new'
+    post 'logout', to: 'devise/sessions#destroy'
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => 'callbacks' }
 end
