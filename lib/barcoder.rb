f0fdfe38791c923efe6576a69182f0f96cd8c930
Michael Romano <micha7a.com@gmail.com>
2018-04-05 17:13:03 -0400

    Add spec and refactor class

--- lib/barcoder.rb
+++ lib/barcoder.rb
@@ -1,40 +1,20 @@
 require 'barby'
 require 'barby/barcode/ean_13'
-require 'barby/outputter/png_outputter'
 require 'barby/outputter/html_outputter'
 
 class Barcoder
   attr_reader :raw_data
 
+  delegate :to_html, to: :html_outputter
+
   def initialize(raw_data)
     @raw_data = raw_data
   end
 
-  def to_html
-    Barby::HtmlOutputter.new(barcode).to_html
-  end
-
-  def to_png
-    unless png_exists?
-      blob = Barby::PngOutputter.new(barcode).to_png
-      File.open(filepath, 'wb'){|f| f.write blob }
-    end
-
-    filename
-  end
-
   private
 
-  def png_exists?
-    File.exists?(filepath)
-  end
-
-  def filepath
-    "app/assets/images/#{filename}"
-  end
-
-  def filename
-    "#{raw_data}-barcode.png"
+  def html_outputter
+    Barby::HtmlOutputter.new(barcode)
   end
 
   def barcode
---
+++ spec/lib/barcoder_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Barcoder do
+  subject { described_class.new(param) }
+
+  describe 'initialize' do
+    context 'when no params are passed' do
+      it 'raises an exception' do
+        expect { described_class.new }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when we pass raw_data' do
+      let(:param) { 123456789012 }
+      it 'initializes correctly' do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'when the data is not 12 digits' do
+      let(:param) { 12 }
+      it 'initializes correctly' do
+        expect { subject }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#to_html' do
+    let(:param) { '123456789012' }
+
+    it 'returns a string without error' do
+      expect(subject.to_html). to be_a String
+    end
+  end
+end
