7143f0d58bf5ab470823e77fef8e89d63256a671
Michael Romano <micha7a.com@gmail.com>
2018-04-08 03:00:55 -0400

    Add mailer and send email when order gets transitioned

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,21 +73,23 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_controller.asset_host = 'https://estomatologia-pucmm.herokuapp.com/'
+  config.action_mailer.asset_host = 'https://estomatologia-pucmm.herokuapp.com/'
+
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'ce.pucmm.edu.do',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
