5e81d5239f0267259cbb7960171dc68b02ee29fa
Michael Romano <micha7a.com@gmail.com>
2018-04-08 14:20:43 -0400

    Add admins controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def after_sign_in_path_for(user)
+    case user.role
+    when :admin
+      users_path
+    when :student
+      orders_path
+    end
+  end
 end
