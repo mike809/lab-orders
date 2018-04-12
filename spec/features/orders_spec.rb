49aca0a18bc455aa29e276015837080f8e9f6fdb
Michael Romano <micha7a.com@gmail.com>
2018-04-12 01:50:37 -0400

    Add feature spec and clear emails before each test

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Orders", type: :feature do
+RSpec.feature "Orders", js: true do
 
   given(:user) { FactoryBot.create(:administrator) }
 
@@ -16,20 +16,43 @@ RSpec.feature "Orders", type: :feature do
     FactoryBot.create(:student, full_name: 'Student Pucmm')
   end
 
-  scenario 'User creates an order' do
-    sign_in user
+  context 'As an authenticated user ' do
+    before do
+      sign_in user
+    end
 
-    visit orders_path
-    click_on('Nueva Orden')
+    scenario 'User creates and receives an order' do
+      visit orders_path
+      click_on('Nueva Orden')
 
-    select('Student Pucmm', from: 'order_student_id')
-    select('Teacher Pucmm', from: 'order_teacher_id')
-    select('Patient Pucmm', from: 'order_patient_id')
+      select('Student Pucmm', from: 'order_student_id')
+      select('Teacher Pucmm', from: 'order_teacher_id')
+      select('Patient Pucmm', from: 'order_patient_id')
 
-    click_button('Crear orden')
-    expect(page).to have_text 'Teacher Pucmm'
-    expect(page).to have_text 'Patient Pucmm'
-    expect(page).to have_text 'Student Pucmm'
-    expect(page.current_path).to eq order_path(Order.last)
+      click_button('Crear orden')
+      expect(page).to have_text 'Teacher Pucmm'
+      expect(page).to have_text 'Patient Pucmm'
+      expect(page).to have_text 'Student Pucmm'
+      expect(page.current_path).to eq order_path(Order.last)
+    end
+
+    scenario 'User receives an order' do
+      visit orders_path
+      click_on('Nueva Orden')
+
+      select('Student Pucmm', from: 'order_student_id')
+      select('Teacher Pucmm', from: 'order_teacher_id')
+      select('Patient Pucmm', from: 'order_patient_id')
+
+      click_button('Crear orden')
+      visit orders_path
+
+      find('tr', text: 'Patient Pucmm Teacher Pucmm Student Pucmm').click
+      expect(page.current_path).to eq edit_order_path(Order.last)
+
+      click_on('Recibir orden')
+      expect(email_count).to eq(1)
+      expect(last_email.body).to have_content('Patient Pucmm')
+    end
   end
 end
