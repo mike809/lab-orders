fea8da8ba38605fa27f3559be22b25013a034220
Michael Romano <micha7a.com@gmail.com>
2018-04-14 19:41:52 -0400

    Add smart listing to users index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,25 @@
 class Admin::UsersController < AdminsController
+  include SmartListing::Helper::ControllerExtensions
+  helper  SmartListing::Helper
+
   def index
-    @users = User.order(created_at: :desc)
+    @students = smart_listing_create :users_with_orders,
+      User.with_orders,
+      partial: 'admin/users/listing',
+      default_sort: { pending_balance: :desc }
+
+    @other_users = smart_listing_create :users_without_orders,
+      User.without_orders,
+      partial: 'admin/users/listing'
+
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
